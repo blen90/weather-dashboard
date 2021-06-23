f3bd3e050602c4be0e6a4e470273a177bb87465e
Blenda Orellana <blen.or90@gmail.com>
2021-06-22 19:39:57 -0500

    working on html file

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//sign up for API
+
+//icons provided to us by api
+//city
+//todays forecast
+//5 day forecast
+
+//city is being stored - not the forecast just the list of cities
+
+//click on the city and reuse function
+
+
+//function that runs api from clicking or typing (used twice)
+
+//API KEY
+
+//
+
+//var weatherApiUrl = "website/city=xxxxxx&apikey=APIKEY"
+
+//use bootstrap to create layout
+
