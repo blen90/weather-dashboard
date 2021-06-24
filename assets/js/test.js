aff5999541aec7137462cacd83907b483259ac93
Blenda Orellana <blen.or90@gmail.com>
2021-06-24 18:35:51 -0500

    added card for today weather, temp, wind and humidity

--- assets/js/test.js
+++ assets/js/test.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     var apiKey = "926ae894b8024b09ca286a6fee4ebb71";
 
     function searchCity(searchValue) {
-        console.log("hey were in searchCity!!!")
+        console.log("hey we're in searchCity!!!")
         console.log(searchValue, "in search city")
         
 
@@ -17,5 +17,41 @@ $(document).ready(function () {
         // var fancyApiUrl =`api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid= ${apiKey}`
         console.log(apiUrl)
 
+        $.ajax({
+            type: "GET",
+            url: apiUrl,
+            dataType: "json",
+            success: function(data){
+                console.log(data)
+
+                var titleEl = $("<h2>").addClass("card-title").text(data.name);
+                var tempEl = $("<h5>").addClass("card-text").text("Temperature: " + data.main.temp + " F");
+                var humidityEl = $("<h5>").addClass("card-text").text("Humidity: " + data.main.humidity + " %");
+                var windEl = $("<h5>").addClass("card-text").text("Wind speed: " +data.wind.speed + " MPH");
+                // var iconEl = $("<div>").addClass("card-image").(data.weather.icon);
+                var card = $("<div>").addClass("card");
+                var cardBody = $("<div>").addClass("card-body");
+                cardBody.append(titleEl, tempEl, humidityEl, windEl);
+                card.append(cardBody)
+                $("#today-weather").append(card);
+            }
+        })
+        
+
     }
 
+
+
+
+    $(".searchBtn").on("click", function (e) {
+        e.preventDefault();
+        console.log("weve been clicked!!")
+        var searchValue = $('#search-city').val()
+        console.log(searchValue)
+        searchCity(searchValue);
+        // searchForecast():
+        // searchUVIndex();
+    })
+
+
+})
