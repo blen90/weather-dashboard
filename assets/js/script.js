123fc918a0c9f2123235c32b6eb5d9329f700d81
Blenda Orellana <blen.or90@gmail.com>
2021-06-27 14:35:39 -0500

    figured out problems with apis, got forecastSearch and uvIndex functions to render info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     console.log("we're working")
 
+    let cityArray = [];
+
     // var date = moment().format("MM/DD/YYYY");
 
     // Set currentDayEl to display currentDay in the header
@@ -79,15 +81,14 @@ $(document).ready(function () {
 
             }
         })
-
     }
 
 
     function forecastSearch(searchValue) {
         console.log("hey we're in searchForecast!!")
-        console.log(searchValue, "in search city")
+        console.log(searchValue, "in searchForecast")
 
-        var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + apiKey;
+        var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue+ "&appid=" + apiKey;
         console.log(forecastUrl)
 
         $.ajax({
@@ -95,55 +96,55 @@ $(document).ready(function () {
             url: forecastUrl,
             dataType: "json",
             success: function (data) {
-                console.log(data)
-            }(function (fiveDay) {
-                for (let i = 0; i != fiveDay.list.length; i += 5) {
-                    let fiveDay = {
-                        date: fiveDay.list[i].dt_txt,
-                        icon: fiveDay.list[i].weather[0].icon,
-                        temp: fiveDay.list[i].main.temp,
-                        humidity: fiveDay.list[i].main.humidity,
-                        wind: fiveDay.list[i].main.wind
-
+                console.log(data.list[2], data.list[10], data.list[18], data.list[26], data.list[32])
+                // console.log("GETTING COORDINATES", data.city.coord)
+                let coordinates = {lat: data.city.coord.lat, long: data.city.coord.lon}
+                uvIndex(coordinates)
+                
+                    for (let i = 0; i < data.list.length; i++) {
+                        let fiveDay = {
+                            date: data.list[i].dt_txt,
+                            icon: data.list[i].weather[0].icon,
+                            temp: data.list[i].main.temp,
+                            humidity: data.list[i].main.humidity,
+                            wind: data.list[i].main.wind
+    
+                        }
+                        
                     }
+    
+                    // var titleEl1 = $("<h2>").addClass("card-title").text(data.name);
+                    // var tempEl1 = $("<h5>").addClass("card-text").text("Temperature: " + data.main.temp + " F");
+                    // var humidityEl1 = $("<h5>").addClass("card-text").text("Humidity: " + data.main.humidity + " %");
+                    // var windEl1 = $("<h5>").addClass("card-text").text("Wind speed: " + data.wind.speed + " MPH");
+                    // // var iconEl = $("<div>").addClass("card-image").(data.weather.icon);
+                    // var card1 = $("<div>").addClass("card");
+                    // var cardBody1 = $("<div>").addClass("card-body");
+                    // cardBody1.append(titleEl1, tempEl1, humidityEl1, windEl1);
+                    // card1.append(cardBody1)
+                    // $("#five-day").append(card);
+    
                 }
-
-                var titleEl1 = $("<h2>").addClass("card-title").text(data.name);
-                var tempEl1 = $("<h5>").addClass("card-text").text("Temperature: " + data.main.temp + " F");
-                var humidityEl1 = $("<h5>").addClass("card-text").text("Humidity: " + data.main.humidity + " %");
-                var windEl1 = $("<h5>").addClass("card-text").text("Wind speed: " + data.wind.speed + " MPH");
-                // var iconEl = $("<div>").addClass("card-image").(data.weather.icon);
-                var card1 = $("<div>").addClass("card");
-                var cardBody1 = $("<div>").addClass("card-body");
-                cardBody1.append(titleEl1, tempEl1, humidityEl1, windEl1);
-                card1.append(cardBody1)
-                $("#five-day").append(card);
-
-
-
-            })
-
+            
         })
-
-    }
+ 
 
     //Uv Index function
 
-    function uvIndex() {
-        console.log(uvIndex, "in uvi");
+    function uvIndex(coordinates) {
+console.log(coordinates, "UV INDEX")
+     
 
-        // var IndexUrl = `https://api.openweathermap.org/v3/uvi/{valueSearch}/{datetime}.json?appid={api_key}`
-        // console.log(IndexUrl);
-
-        var IndexUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=&lon=" + "&appid=" + apiKey;
-        console.log(uvIndex)
+        var IndexUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + coordinates.lat + "&lon=" + coordinates.long + "&appid=" + apiKey;
+        console.log(IndexUrl)
 
         $.ajax({
             type: "GET",
             url: IndexUrl,
             dataType: "json",
             success: function (data) {
-                console.log(data)
+                console.log("UV INDEX DATA", data.value)
+                
             }
         })
     }
@@ -156,9 +157,11 @@ $(document).ready(function () {
 
         searchCity(searchValue);
 
-        forecastSearch();
+        forecastSearch(searchValue);
 
-        uvIndex();
+        cityArray.push(searchValue)
+        console.log(cityArray)
+        
 
     })
 })
